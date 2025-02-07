5b16b0652a029b5c79e831bcaa79b0df666ffaca
Yuyuko-home <misaka_yyz@icloud.com>
2025-02-08 00:00:39 +1100

    fix price formating

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML = '';
 cart.forEach((cartItem) => {
@@ -11,7 +12,7 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   })
-  cartSummaryHTML = 
+  cartSummaryHTML += 
   `<div class="cart-item-container">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
@@ -26,7 +27,7 @@ cart.forEach((cartItem) => {
           ${matchingProduct.name}
         </div>
         <div class="product-price">
-          $${matchingProduct.priceCents / 100}
+          $${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -48,7 +49,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -61,7 +62,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -74,7 +75,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
@@ -92,3 +93,4 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+console.log(cartSummaryHTML);
