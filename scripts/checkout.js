3c00aa0c4cb8717d9c8cae7fb6067fb7ae48a4b3
Yuyuko-home <misaka_yyz@icloud.com>
2025-02-08 00:47:17 +1100

    delete products from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -36,7 +36,8 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link"
+          data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -93,4 +94,14 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-console.log(cartSummaryHTML);
+// console.log(cartSummaryHTML);
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+      // console.log('delete');
+      const productId = link.dataset.productId;
+      // console.log(productId);
+      removeFromCart(productId);
+      console.log(cart);
+    })
+  });
